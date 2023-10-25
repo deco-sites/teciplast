875fdba3cc6f6ce98c326f6cd937cf6457596776
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2023-10-25 15:32:06 -0300

    fix banner grid size images

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,96 +2,98 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$0 from './loaders/Layouts/ProductCard.tsx';
-import * as $$$1 from './loaders/List/Sections.tsx';
-import * as $$$$$$0 from './sections/Gallery.tsx';
-import * as $$$$$$1 from './sections/Footer/Footer.tsx';
-import * as $$$$$$2 from './sections/Category/CategoryBanner.tsx';
-import * as $$$$$$3 from './sections/Category/CategoryList.tsx';
-import * as $$$$$$4 from './sections/Category/CategoryGrid.tsx';
-import * as $$$$$$5 from './sections/Images/BannerCustom.tsx';
-import * as $$$$$$6 from './sections/Images/ShoppableBanner.tsx';
-import * as $$$$$$7 from './sections/Images/BannerGrid.tsx';
-import * as $$$$$$8 from './sections/Images/ImageGallery.tsx';
-import * as $$$$$$9 from './sections/Images/Carousel.tsx';
-import * as $$$$$$10 from './sections/Layout/Container.tsx';
-import * as $$$$$$11 from './sections/Layout/Grid.tsx';
-import * as $$$$$$12 from './sections/Layout/Flex.tsx';
-import * as $$$$$$13 from './sections/Layout/GridItem.tsx';
-import * as $$$$$$14 from './sections/Content/Testimonials.tsx';
-import * as $$$$$$15 from './sections/Content/Logos.tsx';
-import * as $$$$$$16 from './sections/Content/Faq.tsx';
-import * as $$$$$$17 from './sections/Content/Benefits.tsx';
-import * as $$$$$$18 from './sections/Product/Wishlist.tsx';
-import * as $$$$$$19 from './sections/Product/NotFoundChallenge.tsx';
-import * as $$$$$$20 from './sections/Product/SearchResult.tsx';
-import * as $$$$$$21 from './sections/Product/ProductInfo.tsx';
-import * as $$$$$$22 from './sections/Product/ProductShelf.tsx';
-import * as $$$$$$23 from './sections/Product/ImageGallerySlider.tsx';
-import * as $$$$$$24 from './sections/Product/ImageGalleryFrontBack.tsx';
-import * as $$$$$$25 from './sections/Product/ProductShelfTabbed.tsx';
-import * as $$$$$$26 from './sections/Product/NotFound.tsx';
-import * as $$$$$$27 from './sections/Miscellaneous/CampaignTimer.tsx';
-import * as $$$$$$28 from './sections/Miscellaneous/CookieConsent.tsx';
-import * as $$$$$$29 from './sections/Social/WhatsApp.tsx';
-import * as $$$$$$30 from './sections/Social/InstagramPosts.tsx';
-import * as $$$$$$31 from './sections/Theme/Theme.tsx';
-import * as $$$$$$32 from './sections/Links/LinkTree.tsx';
-import * as $$$$$$33 from './sections/Links/Shortcuts.tsx';
-import * as $$$$$$34 from './sections/Newsletter/Newsletter.tsx';
-import * as $$$$$$35 from './sections/Header/Header.tsx';
-import * as $$$$$$$$$$$0 from './apps/decohub.ts';
-import * as $$$$$$$$$$$1 from './apps/site.ts';
+import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
+import * as $$$1 from "./loaders/List/Sections.tsx";
+import * as $$$$$$0 from "./sections/Gallery.tsx";
+import * as $$$$$$1 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$2 from "./sections/Category/CategoryBanner.tsx";
+import * as $$$$$$3 from "./sections/Category/CategoryList.tsx";
+import * as $$$$$$4 from "./sections/Category/CategoryGrid.tsx";
+import * as $$$$$$5 from "./sections/Images/BannerCustom.tsx";
+import * as $$$$$$6 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$7 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$8 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$9 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$10 from "./sections/Layout/Container.tsx";
+import * as $$$$$$11 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$12 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$13 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$14 from "./sections/Content/OurStores.tsx";
+import * as $$$$$$15 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$16 from "./sections/Content/Logos.tsx";
+import * as $$$$$$17 from "./sections/Content/Faq.tsx";
+import * as $$$$$$18 from "./sections/Content/Benefits.tsx";
+import * as $$$$$$19 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$20 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$21 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$22 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$23 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$24 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$25 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$26 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$27 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$28 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$29 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$30 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$31 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$32 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$33 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$34 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$35 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$36 from "./sections/Header/Header.tsx";
+import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
 const manifest = {
-  loaders: {
-    'deco-sites/teciplast/loaders/Layouts/ProductCard.tsx': $$$0,
-    'deco-sites/teciplast/loaders/List/Sections.tsx': $$$1,
+  "loaders": {
+    "deco-sites/teciplast/loaders/Layouts/ProductCard.tsx": $$$0,
+    "deco-sites/teciplast/loaders/List/Sections.tsx": $$$1,
   },
-  sections: {
-    'deco-sites/teciplast/sections/Category/CategoryBanner.tsx': $$$$$$2,
-    'deco-sites/teciplast/sections/Category/CategoryGrid.tsx': $$$$$$4,
-    'deco-sites/teciplast/sections/Category/CategoryList.tsx': $$$$$$3,
-    'deco-sites/teciplast/sections/Content/Benefits.tsx': $$$$$$17,
-    'deco-sites/teciplast/sections/Content/Faq.tsx': $$$$$$16,
-    'deco-sites/teciplast/sections/Content/Logos.tsx': $$$$$$15,
-    'deco-sites/teciplast/sections/Content/Testimonials.tsx': $$$$$$14,
-    'deco-sites/teciplast/sections/Footer/Footer.tsx': $$$$$$1,
-    'deco-sites/teciplast/sections/Gallery.tsx': $$$$$$0,
-    'deco-sites/teciplast/sections/Header/Header.tsx': $$$$$$35,
-    'deco-sites/teciplast/sections/Images/BannerCustom.tsx': $$$$$$5,
-    'deco-sites/teciplast/sections/Images/BannerGrid.tsx': $$$$$$7,
-    'deco-sites/teciplast/sections/Images/Carousel.tsx': $$$$$$9,
-    'deco-sites/teciplast/sections/Images/ImageGallery.tsx': $$$$$$8,
-    'deco-sites/teciplast/sections/Images/ShoppableBanner.tsx': $$$$$$6,
-    'deco-sites/teciplast/sections/Layout/Container.tsx': $$$$$$10,
-    'deco-sites/teciplast/sections/Layout/Flex.tsx': $$$$$$12,
-    'deco-sites/teciplast/sections/Layout/Grid.tsx': $$$$$$11,
-    'deco-sites/teciplast/sections/Layout/GridItem.tsx': $$$$$$13,
-    'deco-sites/teciplast/sections/Links/LinkTree.tsx': $$$$$$32,
-    'deco-sites/teciplast/sections/Links/Shortcuts.tsx': $$$$$$33,
-    'deco-sites/teciplast/sections/Miscellaneous/CampaignTimer.tsx': $$$$$$27,
-    'deco-sites/teciplast/sections/Miscellaneous/CookieConsent.tsx': $$$$$$28,
-    'deco-sites/teciplast/sections/Newsletter/Newsletter.tsx': $$$$$$34,
-    'deco-sites/teciplast/sections/Product/ImageGalleryFrontBack.tsx': $$$$$$24,
-    'deco-sites/teciplast/sections/Product/ImageGallerySlider.tsx': $$$$$$23,
-    'deco-sites/teciplast/sections/Product/NotFound.tsx': $$$$$$26,
-    'deco-sites/teciplast/sections/Product/NotFoundChallenge.tsx': $$$$$$19,
-    'deco-sites/teciplast/sections/Product/ProductInfo.tsx': $$$$$$21,
-    'deco-sites/teciplast/sections/Product/ProductShelf.tsx': $$$$$$22,
-    'deco-sites/teciplast/sections/Product/ProductShelfTabbed.tsx': $$$$$$25,
-    'deco-sites/teciplast/sections/Product/SearchResult.tsx': $$$$$$20,
-    'deco-sites/teciplast/sections/Product/Wishlist.tsx': $$$$$$18,
-    'deco-sites/teciplast/sections/Social/InstagramPosts.tsx': $$$$$$30,
-    'deco-sites/teciplast/sections/Social/WhatsApp.tsx': $$$$$$29,
-    'deco-sites/teciplast/sections/Theme/Theme.tsx': $$$$$$31,
+  "sections": {
+    "deco-sites/teciplast/sections/Category/CategoryBanner.tsx": $$$$$$2,
+    "deco-sites/teciplast/sections/Category/CategoryGrid.tsx": $$$$$$4,
+    "deco-sites/teciplast/sections/Category/CategoryList.tsx": $$$$$$3,
+    "deco-sites/teciplast/sections/Content/Benefits.tsx": $$$$$$18,
+    "deco-sites/teciplast/sections/Content/Faq.tsx": $$$$$$17,
+    "deco-sites/teciplast/sections/Content/Logos.tsx": $$$$$$16,
+    "deco-sites/teciplast/sections/Content/OurStores.tsx": $$$$$$14,
+    "deco-sites/teciplast/sections/Content/Testimonials.tsx": $$$$$$15,
+    "deco-sites/teciplast/sections/Footer/Footer.tsx": $$$$$$1,
+    "deco-sites/teciplast/sections/Gallery.tsx": $$$$$$0,
+    "deco-sites/teciplast/sections/Header/Header.tsx": $$$$$$36,
+    "deco-sites/teciplast/sections/Images/BannerCustom.tsx": $$$$$$5,
+    "deco-sites/teciplast/sections/Images/BannerGrid.tsx": $$$$$$7,
+    "deco-sites/teciplast/sections/Images/Carousel.tsx": $$$$$$9,
+    "deco-sites/teciplast/sections/Images/ImageGallery.tsx": $$$$$$8,
+    "deco-sites/teciplast/sections/Images/ShoppableBanner.tsx": $$$$$$6,
+    "deco-sites/teciplast/sections/Layout/Container.tsx": $$$$$$10,
+    "deco-sites/teciplast/sections/Layout/Flex.tsx": $$$$$$12,
+    "deco-sites/teciplast/sections/Layout/Grid.tsx": $$$$$$11,
+    "deco-sites/teciplast/sections/Layout/GridItem.tsx": $$$$$$13,
+    "deco-sites/teciplast/sections/Links/LinkTree.tsx": $$$$$$33,
+    "deco-sites/teciplast/sections/Links/Shortcuts.tsx": $$$$$$34,
+    "deco-sites/teciplast/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$28,
+    "deco-sites/teciplast/sections/Miscellaneous/CookieConsent.tsx": $$$$$$29,
+    "deco-sites/teciplast/sections/Newsletter/Newsletter.tsx": $$$$$$35,
+    "deco-sites/teciplast/sections/Product/ImageGalleryFrontBack.tsx": $$$$$$25,
+    "deco-sites/teciplast/sections/Product/ImageGallerySlider.tsx": $$$$$$24,
+    "deco-sites/teciplast/sections/Product/NotFound.tsx": $$$$$$27,
+    "deco-sites/teciplast/sections/Product/NotFoundChallenge.tsx": $$$$$$20,
+    "deco-sites/teciplast/sections/Product/ProductInfo.tsx": $$$$$$22,
+    "deco-sites/teciplast/sections/Product/ProductShelf.tsx": $$$$$$23,
+    "deco-sites/teciplast/sections/Product/ProductShelfTabbed.tsx": $$$$$$26,
+    "deco-sites/teciplast/sections/Product/SearchResult.tsx": $$$$$$21,
+    "deco-sites/teciplast/sections/Product/Wishlist.tsx": $$$$$$19,
+    "deco-sites/teciplast/sections/Social/InstagramPosts.tsx": $$$$$$31,
+    "deco-sites/teciplast/sections/Social/WhatsApp.tsx": $$$$$$30,
+    "deco-sites/teciplast/sections/Theme/Theme.tsx": $$$$$$32,
   },
-  apps: {
-    'deco-sites/teciplast/apps/decohub.ts': $$$$$$$$$$$0,
-    'deco-sites/teciplast/apps/site.ts': $$$$$$$$$$$1,
+  "apps": {
+    "deco-sites/teciplast/apps/decohub.ts": $$$$$$$$$$$0,
+    "deco-sites/teciplast/apps/site.ts": $$$$$$$$$$$1,
   },
-  name: 'deco-sites/teciplast',
-  baseUrl: import.meta.url,
+  "name": "deco-sites/teciplast",
+  "baseUrl": import.meta.url,
 };
 
 export type Manifest = typeof manifest;
