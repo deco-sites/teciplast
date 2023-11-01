1e4b33bc1ff480f489ccbb18a20cec85edcef0c9
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2023-11-01 16:59:30 -0300

    ajuste de responsividade da galeria e product shelfs

--- components/search/SearchTerm.tsx
+++ components/search/SearchTerm.tsx
@@ -2,7 +2,7 @@ function SearchTerm() {
   const location = window.location.search;
   const urlParams = new URLSearchParams(location);
   const searchTerm = urlParams.get("q");
-
+  if(!searchTerm) return null;
   return (
     <div>
       <span class="text-2xl">"{searchTerm}"</span>
